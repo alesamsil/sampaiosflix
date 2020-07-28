0220f4a9a5e50ec1d79d6a45c4b25ce0e3160c1d
Alesamsil <alesamsil@gmail.com>
2020-07-28 17:39:30 -0300

    2 aula

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+
 function Menu() {
-return (
-<nav className="Menu">
-<a href="/">
-<img className="Logo" src={Logo} alt="Logo da AluraFlix" />
-</a>
-<a className="ButtonLink" href="/">Novo vídeo</a>
-</nav>
-)
+    return (
+        <nav className="Menu">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
+            </Link>
+
+        <Button as={Link} className="ButtonLink" to="/cadastro/Video">
+            Novo vídeo
+        </Button>
+    </nav>
+    );
 }
 export default Menu
---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroCategoria () {
+    return (
+      <PageDefault>
+        <h1>Página de Cadastro de Categoria</h1>
+
+        <form>
+          <label>
+            Nome da Categoria
+            <input
+              type="text"
+            />
+          </label>
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
+        <Link to="/">
+          Ir pra home
+        </Link>
+      </PageDefault>
+    )
+  }
+  export default CadastroCategoria;
